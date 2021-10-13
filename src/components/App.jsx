059f094f09c23405aa36d0587a80e87cfe854641
Bishal Gautam <bgautam.se@gmail.com>
2021-10-12 22:48:12 -0700

    enter button added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,31 +14,34 @@ export default () => {
 
 
     return (
-    <Canvas>
-        <Suspense fallback={null}>
-            <Shield url="/assets/Shield1/scene.gltf"  />
-            <Environment
-            background={true} // Whether to affect scene.background
-            preset={'forest'} // Preset string (overrides files and path)
-            // scene={undefined} // adds the ability to pass a custom THREE.Scene
+    <>
+        <Canvas>
+            <Suspense fallback={null}>
+                <Shield url="/assets/Shield1/scene.gltf"  />
+                {/* <Environment
+                background={false} // Whether to affect scene.background
+                preset={'forest'} // Preset string (overrides files and path)
+                // scene={undefined} // adds the ability to pass a custom THREE.Scene
+                /> */}
+            </Suspense>
+            <ambientLight intensity={1} />
+            {/* <pointLight position={[10, 10, 10]} /> */}
+            {/* <Stars
+            radius={100} // Radius of the inner sphere (default=100)
+            depth={50} // Depth of area where stars should fit (default=50)
+            count={5000} // Amount of stars (default=5000)
+            factor={10} // Size factor (default=4)
+            saturation={0} // Saturation 0-1 (default=0)
+            fade // Faded dots (default=false)
+            /> */}
+            <OrbitControls
+            enableRotate={true}
+            autoRotate={false}
+            // autoRotateSpeed={0.5}
+            enableZoom={false}
             />
-        </Suspense>
-        <ambientLight intensity={1} />
-        {/* <pointLight position={[10, 10, 10]} /> */}
-        {/* <Stars
-        radius={100} // Radius of the inner sphere (default=100)
-        depth={50} // Depth of area where stars should fit (default=50)
-        count={5000} // Amount of stars (default=5000)
-        factor={10} // Size factor (default=4)
-        saturation={0} // Saturation 0-1 (default=0)
-        fade // Faded dots (default=false)
-        /> */}
-        <OrbitControls
-        enableRotate={true}
-        autoRotate={false}
-        // autoRotateSpeed={0.5}
-        enableZoom={false}
-        />
-    </Canvas>
+        </Canvas>
+        <button id="info">Enter</button>
+    </>
     );
 }
