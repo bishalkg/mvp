2755a189a0bbf960a4923837467ba820f49e3d70
Bishal Gautam <bgautam.se@gmail.com>
2021-10-14 08:53:11 -0700

    can display food or non-food in creatures, need to add toggle button

--- src/views/Creatures.jsx
+++ src/views/Creatures.jsx
@@ -2,25 +2,32 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { NavBar } from '../components/NavBar.jsx';
 import { CreatureList } from './CreatureList.jsx';
+import { MonsterList } from './MonsterList.jsx';
 
 export const Creatures = props => {
-  const [creatures, setCreatures] = useState('');
+  const [creaturesFood, setCreaturesFood] = useState('');
+  const [creaturesNonFood, setCreaturesNonFood] = useState('');
   const [isFetching, setIsFetching] = useState(true);
+  const [toggleCreatures, setToggleCreatures] = useState('food');
 
   useEffect(() => {
     axios.get('https://botw-compendium.herokuapp.com/api/v2/category/creatures')
       .then(({data}) => {
-        setCreatures(data.data);
+        setCreaturesFood(data.data.food);
+        setCreaturesNonFood(data.data.non_food);
         console.log(data.data);
         setIsFetching(false);
       })
       .catch((err) => console.log(err));
   }, []);
 
+
+
   return (
     <div className="creatures-page-container">
       <NavBar />
-      <CreatureList creatures={creatures.food} isFetching={isFetching} />
+      {toggleCreatures === 'food' ? (<CreatureList creatures={creaturesFood} isFetching={isFetching} />) : (<MonsterList monsters={creaturesNonFood} isFetching={isFetching} />)}
+
     </div>
   );
 };
